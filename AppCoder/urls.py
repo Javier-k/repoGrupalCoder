288810f56787c456f3bf159e8ac6760c8bf5aa62
Javier Kaprielian <kaprielianjavier@gmail.com>
2021-12-07 22:17:21 -0300

    Creacion del proyecto

---
+++ AppCoder/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from AppCoder import views
+
+urlpatterns = [
+    path('inicio', views.inicio, name = "Inicio"),
+    path('cursos', views.cursos, name = "Cursos"),
+    path('profesores', views.profesores, name = "Profesores"),
+    path('estudiantes', views.estudiantes, name = "Estudiantes"),
+    path('entregables', views.entregable, name = "Entregables"),
+]
